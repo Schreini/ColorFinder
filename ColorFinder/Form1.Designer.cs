c312246e155ff572aacd0e452e2a4f4323dbadb8
schreiner <thomas.schreiner@fleischpruefring.de>
2017-01-11 15:48:14 +0100

    Initial Checkin

---
+++ ColorFinder/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace ColorFinder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtInput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtOutput = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Color Value (rgb)";
+            // 
+            // TxtInput
+            // 
+            this.TxtInput.Location = new System.Drawing.Point(107, 10);
+            this.TxtInput.Name = "TxtInput";
+            this.TxtInput.Size = new System.Drawing.Size(241, 20);
+            this.TxtInput.TabIndex = 1;
+            this.TxtInput.TextChanged += new System.EventHandler(this.TxtInput_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Color Name";
+            // 
+            // TxtOutput
+            // 
+            this.TxtOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TxtOutput.Location = new System.Drawing.Point(107, 36);
+            this.TxtOutput.Multiline = true;
+            this.TxtOutput.Name = "TxtOutput";
+            this.TxtOutput.ReadOnly = true;
+            this.TxtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TxtOutput.Size = new System.Drawing.Size(702, 381);
+            this.TxtOutput.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(354, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "(Space Separated)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(821, 429);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxtOutput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtInput);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtOutput;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ColorFinder/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ColorFinder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TxtInput_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                var vals = TxtInput.Text.Split(new[]{" "}, StringSplitOptions.RemoveEmptyEntries).ToList();
+                vals.Add("255");
+                vals.Add("255");
+                int r = Convert.ToInt32(vals[0]);
+                int g = Convert.ToInt32(vals[1]);
+                int b = Convert.ToInt32(vals[2]);
+
+                var allColors = Enum.GetValues(typeof (KnownColor))
+                    .Cast<KnownColor>()
+                    .Select(x => new ColorDiff() {Color = Color.FromKnownColor(x), Diff = 1000})
+                    .ToList();
+
+                foreach (var colorDiff in allColors)
+                {
+                      colorDiff.Diff = Math.Abs(colorDiff.Color.R - r)
+                                     + Math.Abs(colorDiff.Color.G - g)
+                                     + Math.Abs(colorDiff.Color.B - b);
+                }
+
+                allColors = allColors.OrderBy(x => x.Diff).ToList();
+                TxtOutput.Clear();
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine(string.Format("Diff\tR\tG\tB\tName"));
+                foreach (var cd in allColors)
+                {
+                    sb.AppendLine(string.Format("{0}\t{1}\t{2}\t{3}\t{4}", cd.Diff, cd.Color.R, cd.Color.G, cd.Color.B, cd.Color.Name));
+                }
+                TxtOutput.Text = sb.ToString();
+                TxtInput.BackColor = Color.LightGreen;
+            }
+            catch (Exception ex)
+            {
+                TxtInput.BackColor = Color.LightCoral;
+                TxtOutput.Clear();
+                TxtOutput.Text = ex.ToString();
+            }
+        }
+    }
+
+    public class ColorDiff
+    {
+        public Color Color;
+        public int Diff;
+    }
+}
